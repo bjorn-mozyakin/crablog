b379693dd2b51f39703071f0e99754386f39682d
Dmitry Rakov <rakov.di@gmail.com>
2019-03-06 13:56:08 +0300

    [+] Add google.analitics script

--- src/php/footer.php
+++ src/php/footer.php
@@ -1,12 +1,23 @@
-  <footer class="footer">
-    <div class="footer__container">© <a href="/"><?php bloginfo('name'); ?></a>
-      2016-<?php echo date('Y');?><!--<?php the_time('Y'); ?>-->
+    <footer class="footer">
+      <div class="footer__container">© <a href="/"><?php bloginfo('name'); ?></a>
+        2016-<?php echo date('Y');?><!--<?php the_time('Y'); ?>-->
+      </div>
+      <div class="arrow-up arrow-up_hidden"></div>
+    </footer>
+
+
+    <?php wp_footer(); ?>
     </div>
-    <div class="arrow-up arrow-up_hidden"></div>
-  </footer>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-135689189-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
 
-  <?php wp_footer(); ?>
-  </div>
-</body>
+      gtag('config', 'UA-135689189-1');
+    </script>
+
+  </body>
 </html>
